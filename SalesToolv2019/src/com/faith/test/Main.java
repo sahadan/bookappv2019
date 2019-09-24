861e7ad8200cdf6914775eec5d6613a4ae4e9f53
Sahadan <sahadan@gmail.com>
2019-09-24 12:07:43 +0530

    Initial Commit

---
+++ SalesToolv2019/src/com/faith/test/Main.java
@@ -0,0 +1,20 @@
+package com.faith.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		SalesData objSalesData=new SalesData();
+		System.out.println("Welcome to Sales App");
+		displayGreeting();
+		
+		objSalesData.display();
+	}
+	
+	private static void displayGreeting() {
+		
+		System.out.println("Hi guys! Sales report shows the latest info");
+		
+	}
+
+}
